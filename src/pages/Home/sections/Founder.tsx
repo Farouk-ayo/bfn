a39a74430f7209a562bb8b7ac8d3df2bc05f6f20
Farouk-ayo <mustaphafarouk41@gmail.com>
2024-09-25 09:48:25 +0100

    design fix

--- src/pages/Home/sections/Founder.tsx
+++ src/pages/Home/sections/Founder.tsx
@@ -89,7 +89,7 @@ const Founder: React.FC = () => {
           </button>
         </div>
 
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 ">
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 px-6 sm:px-0 ">
           {foundersToShow.map((founder, index) => (
             <div
               key={index}
